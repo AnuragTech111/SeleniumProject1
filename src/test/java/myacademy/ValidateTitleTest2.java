650943721c53064fa2fae362223ba66fe0fe015a
AnuragTech111 <anuragtech111@gmail.com>
2021-01-08 16:16:32 +0530

    Created new developA branch and ValidateTitleTest2 file

---
+++ src/test/java/myacademy/ValidateTitleTest2.java
@@ -0,0 +1,54 @@
+package myacademy;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.*;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pageObjects.LandingHomePage;
+import pageObjects.LoginPage;
+import resources.Base;
+
+public class ValidateTitleTest2 extends Base{
+	
+	private static Logger log=LogManager.getLogger(ValidateTitleTest2.class.getName());
+	
+	@BeforeTest
+	public void initialize() throws IOException
+	{
+		driver=driverInitialization();
+		// driver.get("https://rahulshettyacademy.com/#/index");
+		driver.get(prop.getProperty("home_page_url"));
+		log.info("Home Page is displayed successfully");
+	}
+
+
+	@Test
+	public void titleCourseValidation() throws IOException
+	{
+		
+		LandingHomePage landingHome=new LandingHomePage(driver);
+		//landingHome.getTitleCourse().getText();
+		Assert.assertEquals(landingHome.getTitleCourse().getText(), "Featured Courses");
+		log.info("Correct Title is displayed successfully");
+		
+		//Assert.assertFalse(landingHome.getRegisterLink().isDisplayed());
+		//Assert.assertTrue(landingHome.getRegisterLink().isDisplayed());
+		
+		
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() 
+	{
+		driver.close();
+		driver=null;
+	}
+
+}
